90339c43c5ad52260f7f142fa26f38e9a5dd1228
Himadri Talukder <himadricuet@gmail.com>
2017-12-13 09:29:21 -0500

    Thymeleaf view setup and initial configuration

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,17 @@
 package com.example.demo;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/hi")
-	public @ResponseBody String hiThere() {
-		return "Hello there!";
+	@RequestMapping("/hi/{name}")
+	public String hiThere(Map model, @PathVariable String name) {
+		model.put("name", name);
+		return "hello";
 	}
 }
