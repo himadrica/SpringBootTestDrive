4fb10ff7a25cac1f4206ed7b72750375879c07ae
Himadri Talukder <himadricuet@gmail.com>
2017-12-12 22:06:17 -0500

    Running as a WAR packaging configuration

--- src/main/java/com/example/demo/MicroservicesBootApplication.java
+++ src/main/java/com/example/demo/MicroservicesBootApplication.java
@@ -2,11 +2,24 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MicroservicesBootApplication {
+public class MicroservicesBootApplication extends SpringBootServletInitializer{
 
+	/**
+	 * Used when running as a JAR
+	 * **/
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicesBootApplication.class, args);
+	}
+
+	/**
+	 * Used when running as a WAR
+	 * **/
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MicroservicesBootApplication.class);
 	}
 }
