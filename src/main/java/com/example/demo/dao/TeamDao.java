cdaab8ef1e19d77457386fa036fed4d6fc7378a9
Himadri Talukder <himadricuet@gmail.com>
2017-12-13 19:29:09 -0500

    Spring data REST example from DAO repository, HATEOAS

--- src/main/java/com/example/demo/dao/TeamDao.java
+++ src/main/java/com/example/demo/dao/TeamDao.java
@@ -2,8 +2,11 @@ package com.example.demo.dao;
 
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+
 import com.example.demo.domain.Team;
 
+@RestResource(path="teams", rel="teams")
 public interface TeamDao extends CrudRepository<Team, Long> {
 
 	List<Team> findAll();
